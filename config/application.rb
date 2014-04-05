5bd0930c48f74da56d82ccbb8332f1c733a844c8
shin_semiya <jun_you@live.jp>
2014-04-06 00:16:53 +0900

    replace default locale to japanese

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module Koreyomo
     # lib以下をautoloadする
     config.autoload_paths += %W(#{config.root}/lib)
     config.autoload_paths += Dir["#{config.root}/lib/**/"]
+    config.i18n.default_locale = :ja
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
